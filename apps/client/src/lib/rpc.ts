92da124166de9be6e37f9258e5ce9404a89a437b
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-10-18 09:09:31 +0545

    chore: Update import path for AppType in rpc.ts

--- apps/client/src/lib/rpc.ts
+++ apps/client/src/lib/rpc.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
-import { AppType } from "../../../server/src/app"
+import { AppType } from "../../../server/dist/src/app"
 export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!);
 
