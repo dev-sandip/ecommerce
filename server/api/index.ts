80b6185e32b36a7f15fd05576724411a4b297b81
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-10-24 11:54:05 +0545

    fix(server):deployment issues

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,6 @@
 import { handle } from "@hono/node-server/vercel";
 
 // eslint-disable-next-line antfu/no-import-dist
-import app from "../dist/src/index.js";
+import app from "../dist/src/index";
 
 export default handle(app);
